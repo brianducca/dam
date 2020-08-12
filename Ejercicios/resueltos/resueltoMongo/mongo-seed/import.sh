9fbe5dc83f55f353c9e23df646cada11e83c58e0
Brian Ducca <brian.ducca@gmail.com>
2020-08-12 18:48:48 -0300

    Agrego comentario sobre fix en caso de que les de permission denied

--- Ejercicios/resueltos/resueltoMongo/mongo-seed/import.sh
+++ Ejercicios/resueltos/resueltoMongo/mongo-seed/import.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-
+#Revisar si este archivo tiene permisos de ejecución, sino correr  chmod +x import.sh
 mongoimport --host mongo --port 27017 --authenticationDatabase admin --username root --password rootPass --db DAM --collection Usuarios --type json --file /mongo-seed/import.json --jsonArray
