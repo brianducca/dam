95c38e69a5906904f058ea38b8265cffb62a7668
Brian Ducca <bducca@agn.gov.ar>
2020-10-29 17:28:54 -0300

    Actualizo material, la clase 4 y el resuelto de mongo

--- Ejercicios/resueltos/resueltoMongo/src/usuarios/index.js
+++ Ejercicios/resueltos/resueltoMongo/src/usuarios/index.js
@@ -61,4 +61,17 @@ routerUsuario.get('/10', function(req, res) {
     });
 });
 
+
+//10
+routerUsuario.put('/actualizar', function(req, res) {
+    console.log(mongo.db);
+    mongo.db.collection("Usuarios").update({ _id: mongo.objectId('5f19513a918d509af3823df6') }, { $set: { nombre: "nuevoNombre" } }, function(err, docs) {
+        if (err) {
+            res.send(err).status(400);
+            return;
+        }
+        res.send(docs);
+    });
+});
+
 module.exports = routerUsuario;
