aff077e7ad41c24fe0042915ba7c622a0cd43d79
Leonardo Mancini <leonardomancini82@gmail.com>
2022-04-20 22:29:53 -0300

    Add endpoint to create and delete dispositivos in backend

--- TP/backend/routes/dispositivos/index.js
+++ TP/backend/routes/dispositivos/index.js
@@ -4,7 +4,7 @@ var pool = require('../../mysql');
 
 //Devuelve un array de dispositivos
 routerDispositivo.get('/', function(req, res) {
-        pool.query('Select * from Dispositivos', function(err, result, fields) {
+        pool.query('Select * from Dispositivos WHERE deleted = false', function(err, result, fields) {
                 if (err) {
                     res.send(err).status(400);
                     return;
@@ -14,7 +14,7 @@ routerDispositivo.get('/', function(req, res) {
 });
 
 routerDispositivo.get('/:id', function(req, res) {
-        let query = 'Select * from Dispositivos d  WHERE d.dispositivoId = ?'
+        let query = 'Select * from Dispositivos d  WHERE d.dispositivoId = ? AND d.deleted = false'
         pool.query(query, [req.params.id], function(err, result, fields) {
                 if (err) {
                     res.send(err).status(400);
@@ -48,6 +48,37 @@ routerDispositivo.get('/:id/mediciones', function(req, res) {
                 }
                 res.send(result);
             });
+});
+
+routerDispositivo.post("/", function(req, res) {
+    let insertElectroSQL = "INSERT INTO Electrovalvulas (nombre) VALUES (?)";
+    pool.query(insertElectroSQL, [req.body.electrovalvula],(error, result, fields) => {
+        if(error) return res.status(500).send("Error:"+ error);
+        let electroId = result.insertId;
+        let insertSQL = "INSERT INTO Dispositivos (nombre,ubicacion,electrovalvulaId) VALUES (?,?,?)";
+        pool.query(insertSQL, [req.body.nombre, req.body.ubicacion,electroId],(errorInsertDisp, resultInsertDisp, fields) => {
+            if(errorInsertDisp) return res.status(500).send("Error:"+ errorInsertDisp);
+                return res.status(201).send({
+                    electrovalvulaId:electroId,
+                    dispositivoId:resultInsertDisp.insertId
+            });;
+        });
+    });
+});
+
+routerDispositivo.delete("/:id", function(req,res) {
+    let sql = "UPDATE Dispositivos set deleted = true WHERE dispositivoId = ?";
+
+    
+
+    pool.query(sql, [req.params.id], function(err, result, fields) {
+        if (err) {
+            res.send(err).status(400);
+            return;
+        }
+        res.send(result);
+    });
+
 });
 
 
